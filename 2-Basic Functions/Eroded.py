fb9d24c1bf5c3e6ca36dc52fdaa050fc5f768e06
vishwas9699 <vvishwas179@gmail.com>
2020-07-17 20:24:07 +0530

    ic

--- 2-Basic Functions/Eroded.py
+++ 2-Basic Functions/Eroded.py
@@ -6,7 +6,9 @@ Kernal = np.ones((5,5),np.uint8)
 
 imgCanny = cv2.Canny(img,150,200)
 imgDialation = cv2.dilate(imgCanny,Kernal,iterations=1)
+imgEroded = cv2.erode(imgDialation,Kernal,iterations=1)
 
 cv2.imshow("Canny image",imgCanny)
 cv2.imshow("Dialation image",imgDialation)
+cv2.imshow("Eroded image",imgEroded)
 cv2.waitKey(0)
