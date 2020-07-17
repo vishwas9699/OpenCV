830f02a134241dc817cbd1c4842fc2df022993b5
vishwas9699 <vvishwas179@gmail.com>
2020-07-17 23:50:11 +0530

    ic

--- 6-Joining Images/Joining-images.py
+++ 6-Joining Images/Joining-images.py
@@ -38,11 +38,11 @@ imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
  
 imgStack = stackImages(0.5,([img,imgGray,img],[img,img,img]))
  
-# imgHor = np.hstack((img,img))
-# imgVer = np.vstack((img,img))
-#
-# cv2.imshow("Horizontal",imgHor)
-# cv2.imshow("Vertical",imgVer)
+imgHor = np.hstack((img,img))
+imgVer = np.vstack((img,img))
+
+cv2.imshow("Horizontal",imgHor)
+cv2.imshow("Vertical",imgVer)
 cv2.imshow("ImageStack",imgStack)
  
-cv2.waitKey(0)
+cv2.waitKey(3000)
