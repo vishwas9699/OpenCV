83c01b30809ba4c5aafb782b00182fd5af17e53c
vishwas9699 <vvishwas179@gmail.com>
2020-07-18 20:21:26 +0530

    ic

--- 9-Face Detection/Face-Detection.py
+++ 9-Face Detection/Face-Detection.py
@@ -7,7 +7,7 @@ imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 faces = faceCascade.detectMultiScale(imgGray,1.9,4)
  
 for (x,y,w,h) in faces:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,100),2)
  
 cv2.imshow("Result", img)
 cv2.waitKey(0)
