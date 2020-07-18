f41d153172278b7f570a97b9846888e9d53fb2f2
vishwas9699 <vvishwas179@gmail.com>
2020-07-18 09:12:16 +0530

    ic

--- 7-Colour Detection/Clour-detection.py
+++ 7-Colour Detection/Clour-detection.py
@@ -1,8 +1,11 @@
 import cv2
 
 path = "Resources\lambo.png"
-img = cv2.imread(path)
 
+img = cv2.imread(path)
+ImgHSV=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 
 cv2.imshow("Original Output",img)
+cv2.imshow("HSV Output",ImgHSV)
+
 cv2.waitKey(0)
