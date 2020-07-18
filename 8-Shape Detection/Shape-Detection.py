8c31e3884b9c270d62cc58c2f57bf0a93bc90d6a
vishwas9699 <vvishwas179@gmail.com>
2020-07-18 16:31:02 +0530

    ic

--- 8-Shape Detection/Shape-Detection.py
+++ 8-Shape Detection/Shape-Detection.py
@@ -38,27 +38,27 @@ def getContours(img):
         area = cv2.contourArea(cnt)
         print(area)
         if area>500:
-            cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 3)
+            cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 2)
             peri = cv2.arcLength(cnt,True)
-            #print(peri)
+            print(peri)
             approx = cv2.approxPolyDP(cnt,0.02*peri,True)
             print(len(approx))
             objCor = len(approx)
-            x, y, w, h = cv2.boundingRect(approx)
+            x,y,w,h = cv2.boundingRect(approx)
  
-            if objCor ==3: objectType ="Tri"
+            if objCor ==3: objectType ="Triangle"
             elif objCor == 4:
                 aspRatio = w/float(h)
                 if aspRatio >0.98 and aspRatio <1.03: objectType= "Square"
                 else:objectType="Rectangle"
-            elif objCor>4: objectType= "Circles"
+            elif objCor>4: objectType= "Circle"
             else:objectType="None"
  
  
  
             cv2.rectangle(imgContour,(x,y),(x+w,y+h),(0,255,0),2)
             cv2.putText(imgContour,objectType,
-                        (x+(w//2)-10,y+(h//2)-10),cv2.FONT_HERSHEY_COMPLEX,0.7,
+                        (x+(w//2)-10,y+(h//2)-10),cv2.FONT_HERSHEY_COMPLEX,0.6,
                         (0,0,0),2)
  
  
@@ -68,7 +68,7 @@ imgContour = img.copy()
  
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGray,(7,7),1)
-imgCanny = cv2.Canny(imgBlur,50,50)
+imgCanny = cv2.Canny(imgGray,50,50)
 getContours(imgCanny)
  
 imgBlank = np.zeros_like(img)
