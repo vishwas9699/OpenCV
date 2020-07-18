17379af92ca3febec26df6c1012d25c79931f3f7
vishwas9699 <vvishwas179@gmail.com>
2020-07-18 11:01:30 +0530

    ic

--- 7-Colour Detection/Clour-detection.py
+++ 7-Colour Detection/Clour-detection.py
@@ -17,7 +17,12 @@ cv2.createTrackbar("Value Maximum","TrackBars",255,255,empty)
 while True:
     img = cv2.imread(path)
     imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-
+    h_min = cv2.getTrackbarPos("Hue Minimum","TrackBars")
+    h_max = cv2.getTrackbarPos("Hue Maximum","TrackBars")
+    s_min = cv2.getTrackbarPos("Saturation Minimum","TrackBars")
+    s_max = cv2.getTrackbarPos("Saturation Maximum","TrackBars")
+    v_min = cv2.getTrackbarPos("Value Minimum","TrackBars")
+    v_max = cv2.getTrackbarPos("Value Maximum","TrackBars")
 
 
     cv2.imshow("Original Image",img)
